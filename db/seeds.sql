53579cc872b5abf14a165985906cb67e47fe6335
cdfoye <cdfoye@gmail.com>
2022-05-13 20:51:31 -0600

    created seed values

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name)
+VALUES ("HR"),
+       ("IT"),
+       ("Marketing"),
+       ("Web Development");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("HR Manager", 80000, 1),
+       ("Junior Web Developer", 85000, 4),
+       ("IT Technician", 70000, 2),
+       ("Social Media Specialist", 65000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Meridith", "Grey", 1,),
+       ("Darth", "Vader", 3),
+       ("Spongebob", "Squarepants", 4),
+       ("Patrick", "Star", 2);
+       
